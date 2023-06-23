4d9ecdc38651dc206f3e1d988273f2bbf399d193
Sadaleksej <135204154+Sadaleksej@users.noreply.github.com>
2023-06-23 17:06:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+int[] arrr = new int[30];
+FilArray(arrr);
+PrintArray(arrr);
+Console.WriteLine();
+Console.WriteLine($"Количество нечетных: {NumbersEven(arrr)}");
+
+void FilArray(int[] ar)
+{
+    for (int i = 0; i < ar.Length; i++) ar[i] = new Random().Next(100, 999);
+}
+
+void PrintArray(int[] ar)
+{
+    for (int i = 0; i < ar.Length; i++) Console.Write(ar[i] + " ");
+}
+int NumbersEven(int[] ar)
+{
+    int num = 0;
+  foreach (int el in ar) 
+    {
+    if(el % 2 == 0) num++ ;
+    }
+return num;
+}
+
+
